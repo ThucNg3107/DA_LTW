60bb7bb9380ea99e865ca2941e1b0aa0e0ed1a3c
thucnguyenhien102 <thucnguyenhien102@gmail.com>
2025-04-06 02:25:30 +0700

    up

--- QuanLyThuVien/QuanLyThuVien/obj/Debug/net8.0/QuanLyThuVien.AssemblyInfo.cs
+++ QuanLyThuVien/QuanLyThuVien/obj/Debug/net8.0/QuanLyThuVien.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a3a7b7fadd4c00eb10b493a5a37b540d042066")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
